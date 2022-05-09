de3265e15500e803ee7fd508e9d997b1e5db7e37
bomi kim <ekki88@naver.com>
2022-05-09 23:47:04 +0900

    f

--- function_main.js
+++ function_main.js
@@ -1,13 +1,12 @@
-const num = 1;
-const num2 = 2;
-const result = num +num2;
-console.log(result);
-
-const num3 = 1;
-const num4 = 2;
-const result = num3 + num4 ;
 
 
 function add(a,b){
     return a+b;
 }
+
+function surprise (operator) {
+    const result = operator(2,3);
+    console.log(result);
+}
+
+surprise (add);
