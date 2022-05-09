944c3ab7e4ef416c502fc044be9e2b932f952bf4
bomi kim <ekki88@naver.com>
2022-05-09 23:26:17 +0900

    f

---
+++ function_main.js
@@ -0,0 +1,13 @@
+const num = 1;
+const num2 = 2;
+const result = num +num2;
+console.log(result);
+
+const num3 = 1;
+const num4 = 2;
+const result = num3 + num4 ;
+
+
+function add(a,b){
+    return a+b;
+}
